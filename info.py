5b35fb5827df921697c5ab59eb7b0f5ec7fbe55f
ashishpatel8583-ctrl <ashishpatel8583@gmail.com>
2026-01-23 09:21:14 +0530

    Update GRP_LNK environment variable URL

--- info.py
+++ info.py
@@ -50,7 +50,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'SilentXBotz_files')
 MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "False"), False) # Type True For Turn On MULTIPLE DB FUNTION 
 DATABASE_URI2 = environ.get('DATABASE_URI2', "")
 
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+Ua1FILtt1AMxZmJl')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+UXpCiRa0ZTM5Zjg1')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+jyw-yHhMQWs2MDc1')
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/mrperfectjikabot')
 UPDATE_CHANNEL_LNK = environ.get('UPDATE_CHANNEL_LNK', 'https://t.me/+8Kes2tN8I1NmOTll')
