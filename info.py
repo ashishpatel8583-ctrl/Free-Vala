1406cf3cc8c26ecc104b48fb32498624f3e3e2d5
ashishpatel8583-ctrl <ashishpatel8583@gmail.com>
2026-02-10 00:50:01 -0800

    Update info.py

--- info.py
+++ info.py
@@ -63,8 +63,8 @@ CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+jyw-yHhMQWs2MDc1')
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/mrperfectjikabot')
 UPDATE_CHANNEL_LNK = environ.get('UPDATE_CHANNEL_LNK', 'https://t.me/+Cgm91SVI5m1mODY1')
 
-AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '-1002459554188'))
-AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '-1002459554188'))
+AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '-1003569663484'))
+AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '-1003569663484'))
 
 IS_VERIFY = is_enabled('IS_VERIFY', False)
 LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1003383191711'))
