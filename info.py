5e2186921c8e4f19d190ae0d7ed26237e0ee68c5
ashishpatel8583-ctrl <ashishpatel8583@gmail.com>
2026-02-10 03:32:45 -0800

    Update info.py

--- info.py
+++ info.py
@@ -20,28 +20,28 @@ SESSION = environ.get('SESSION', 'media_search')
 API_HASH = environ.get('API_HASH', 'b9eaaeead349eb9c593bfe9ae04ded7d')
 
 
-#################################################################################################
+############################################   Sᴛᴇᴘ Nᴏ  1    #####################################################################################################
 
 IS_VERIFY = is_enabled('IS_VERIFY', False)
 EMOJI_MODE = bool(environ.get('EMOJI_MODE', False))
 LINK_MODE = is_enabled((environ.get('LINK_MODE', "False")), True)
 
 
-###############################################################################################################################################
+#############################################   Sᴛᴇᴘ Nᴏ 2    #####################################################################################################
 # you can set same log channel id  in all of this fill in the blank
 
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1003649675709'))  
 BIN_CHANNEL = int(environ.get('BIN_CHANNEL', '  -1003649675709'))  
 PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1003649675709'))
 
-###################################################################################################################################################
+###########################################     Sᴛᴇᴘ Nᴏ 3    ######################################################################################################
 # koi verify karega to Notification yhi pr aayega Bhai Log Shamjhe
 
 LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1003383191711'))
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1003383191711'))
 MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002715836361'))
 
-####################################################################################################################################################
+#############################################    Sᴛᴇᴘ Nᴏ 4   ######################################################################################################
 # Yha Pr Group Or Channel Ka Link Dena Ye Jaruri Hau Bhai Log To Jarur Dena 
 
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+uuFJAqVED45hOTRl')
@@ -51,19 +51,19 @@ TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/+sYYpPgyyOq03Mzhl")
 TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/+sYYpPgyyOq03Mzhl")
 UPDATE_CHANNEL_LNK = environ.get('UPDATE_CHANNEL_LNK', 'https://t.me/+vDrf9yVGLJ5hZTll')
 
-########################################################################################################################################################
+###############################################     Sᴛᴇᴘ Nᴏ 5     #################################################################################################
 # Force Subscribe Channel Pahle Vale Me doge To Normal Join Mode Dono Me Same Doge To Request Mode Normal Ke Liye Dusre Vale Me Update Channel Ka Id Do 
 
 AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '-1002459554188'))
 AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '-1002459554188'))
 
-########################################################################################################################################################
+###############################################     Sᴛᴇᴘ Nᴏ 6     #################################################################################################
 #channel Vale me Database Channel ka id Dena Hai Bhai Log Koi bhi glti mt krna Aap Log
 
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1982405151 7840839334').split()] 
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1003127102322').split()]
 
-#########################################################################################################################################################
+###############################################     Sᴛᴇᴘ Nᴏ 7     #################################################################################################
 # Mongob Ka Hi Database Url Dena Bhai Log
 
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://merchantnavy:merchantnavy@cluster0.19dbvu7.mongodb.net/?appName=Cluster0")
@@ -74,7 +74,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'SilentXBotz_files')
 MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "False"), False) # Type True For Turn On MULTIPLE DB FUNTION 
 DATABASE_URI2 = environ.get('DATABASE_URI2', "")
 
-#########################################################################################################################################################
+#################################################     Sᴛᴇᴘ Nᴏ 8     ################################################################################################
 
 SHORTENER_API = environ.get("SHORTENER_API", "bc9e90eb5dfdd6a3a7a6e61cef1e2e3de23b922f")
 SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "shortxlinks.in")
