0ac7b26441187925d143ad6279d379f3b9280c76
ashishpatel8583-ctrl <ashishpatel8583@gmail.com>
2026-02-13 07:12:47 +0530

    Fix button label from 'Upgrade' to 'Premium'

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1207,7 +1207,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     InlineKeyboardButton('+ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ +', url=f'http://telegram.me/{temp.U_NAME}?startgroup=true')
                 ],[
                     InlineKeyboardButton('🧧 ᴛʀᴇɴᴅɪɴɢ ', callback_data="topsearch"),
-                    InlineKeyboardButton('🎟️ ᴜᴘɢʀᴀᴅᴇ ', callback_data="premium"),
+                    InlineKeyboardButton('🎟️ ᴘʀᴇᴍɪᴜᴍ ', callback_data="premium"),
                 ],[
                     InlineKeyboardButton('♻️ ᴅᴍᴄᴀ', callback_data='disclaimer'),
                     InlineKeyboardButton('👤 ᴀʙᴏᴜᴛ ', callback_data='me')
